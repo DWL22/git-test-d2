525ca13b6cbfdaac3ae0ad075355fdd266b53a06
DWL22 <candra.icy@gmail.com>
2024-03-20 15:29:41 +0800

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque id vero neque esse nemo, ex quos modi recusandae temporibus accusamus maxime? Dolorum fugit quod quisquam, delectus sit ea deleniti doloribus?</p>
 </body>
 </html>
